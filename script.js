c8ec06a1240ac7abf37bc459bbe0bae72d90c7e8
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-02-20 07:09:37 +0200

    WIP Add texts.

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ angular.module('galleryApp', [])
     var gallery = this;
     gallery.language = "ro";
     gallery.work_start_date = new Date(2017,02,12);
-
-    gallery.worked_hours = 7;  // EDIT HERE
+    gallery.worked_hours_a = 6;  // EDIT HERE
+    gallery.worked_hours_g = 1;  // EDIT HERE
+    gallery.worked_hours = gallery.worked_hours_a + gallery.worked_hours_g;
 
     gallery.get_global_progress = function() {
       var index;
@@ -125,324 +126,468 @@ angular.module('galleryApp', [])
       {
         id: 0,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Coperta",
+          en: "Cover"
+        },
         text: {
-          ro: "Text aici",
-          en: "Text here"
+          ro: "Coperta cărții (color)",
+          en: "Book cover (color)"
         }
       },
       {
         id: 1,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Apa transformată în vin",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Ioan 2:1-11",
           en: "Text here"
         }
       },
       {
         id: 2,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea fiului slujbașului împărătesc",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Ioan 4:46-53",
           en: "Text here"
         }
       },
       {
         id: 3,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Pescuirea minunată",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Luca 5:1-11",
           en: "Text here"
         }
       },
       {
         id: 4,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea leprosului",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Marcu 1:40-45",
           en: "Text here"
         }
       },
       {
         id: 5,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea robului sutașului roman",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Luca 7:1-10",
           en: "Text here"
         }
       },
       {
         id: 6,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Învierea fiului văduvei din Nain",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Luca 7:11-17",
           en: "Text here"
         }
       },
       {
         id: 7,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea îndrăcitului la Capernaum",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Luca 4:31-37",
           en: "Text here"
         }
       },
       {
         id: 8,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea soacrei lui Petru",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Marcu 1:29-31",
           en: "Text here"
         }
       },
       {
         id: 9,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea sălăbnogului",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Luca 5:17-26",
           en: "Text here"
         }
       },
       {
         id: 10,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea slăbănogului la Betesda",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Ioan 5:1-9",
           en: "Text here"
         }
       },
       {
         id: 11,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea omului cu mâna uscată",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Luca 6:6-11",
           en: "Text here"
         }
       },
       {
         id: 12,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea unui îndrăcit mut și orb",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Matei 12:22-23",
           en: "Text here"
         }
       },
       {
         id: 13,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Potolirea furtunii",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Luca 8:22-25",
           en: "Text here"
         }
       },
       {
         id: 14,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea celor doi îndrăciți",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Matei 8:28-34",
           en: "Text here"
         }
       },
       {
         id: 15,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Învierea fiicei lui Iair",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Marcu 5:35-43",
           en: "Text here"
         }
       },
       {
         id: 16,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea femeii cu scurgere de sânge",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Luca 8:43-48",
           en: "Text here"
         }
       },
       {
         id: 17,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea celor doi orbi",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Matei 9:27-31",
           en: "Text here"
         }
       },
       {
         id: 18,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea unui mut îndrăcit",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Matei 9:32-34",
           en: "Text here"
         }
       },
       {
         id: 19,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Hrănirea celor cinci mii de oameni",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Ioan 6:1-14",
           en: "Text here"
         }
       },
       {
         id: 20,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Umblarea pe mare",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Matei 14:22-33",
           en: "Text here"
         }
       },
       {
         id: 21,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea fiicei femeii canaanite",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Matei 15:21-28",
           en: "Text here"
         }
       },
       {
         id: 22,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea omului surd și mut",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Marcu 7:31-37",
           en: "Text here"
         }
       },
       {
         id: 23,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Hrănirea celor patru mii de oameni",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Matei 15:29-39",
           en: "Text here"
         }
       },
       {
         id: 24,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea orbului la Betsaida",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Marcu 8:22-26",
           en: "Text here"
         }
       },
       {
         id: 25,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea unui copil lunatic",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Marcu 9:14-27",
           en: "Text here"
         }
       },
       {
         id: 26,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Moneda din gura peștelui",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Matei 17:24-27",
           en: "Text here"
         }
       },
       {
         id: 27,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea orbului din naștere",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Ioan 9:1-14",
           en: "Text here"
         }
       },
       {
         id: 28,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea femeii gârbove",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Luca 13:10-13 (14-16?)",
           en: "Text here"
         }
       },
       {
         id: 29,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea unui bolnav de dropică",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Luca 14:1-4 (5-6?)",
           en: "Text here"
         }
       },
       {
         id: 30,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea celor zece leproși",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Luca 17:11-19",
           en: "Text here"
         }
       },
       {
         id: 31,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Învierea lui Lazar",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Ioan 11:1-46?",
           en: "Text here"
         }
       },
       {
         id: 32,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea celor doi orbi cerșetori",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Matei 20:29-34 & Marcu 10:46-52?",
           en: "Text here"
         }
       },
       {
         id: 33,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Blestemarea smochinului",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Matei 21:18-22 & Marcu 11: 12-19?",
           en: "Text here"
         }
       },
       {
         id: 34,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "Vindecarea urechii lui Malhu",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Luca 22:47-51?",
           en: "Text here"
         }
       },
       {
         id: 35,
         img: 'ZZ.png',
-        done: 0,
+        done: 5,
+        title: {
+          ro: "A doua pescuire minunată",
+          en: "Text here"
+        },
         text: {
-          ro: "Text aici",
+          ro: "Ioan 21:1-13",
           en: "Text here"
         }
       }
