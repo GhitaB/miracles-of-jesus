d9eeda97f74bc1b94cf83341c5fd69f87a6a7a8d
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-03-09 08:56:32 +0200

    Update 34.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ angular.module('galleryApp', [])
     var gallery = this;
     gallery.language = "ro";
     gallery.work_start_date = new Date(2017,02,12);
-    gallery.worked_hours = 37;  // EDIT HERE
+    gallery.worked_hours = 37.5;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
@@ -565,8 +565,8 @@ angular.module('galleryApp', [])
       },
       {
         id: 34,
-        img: '34.jpg',
-        done: 20,
+        img: '34.png',
+        done: 50,
         title: {
           ro: "Vindecarea urechii lui Malhu",
           en: "Restoring the ear of Malhu"
