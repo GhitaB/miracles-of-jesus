cf232a7369cd35a045073bfd0cb33372a9beb607
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-03-13 09:36:45 +0200

    Update 33.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ angular.module('galleryApp', [])
     var gallery = this;
     gallery.language = "ro";
     gallery.work_start_date = new Date(2017,02,12);
-    gallery.worked_hours = 38;  // EDIT HERE
+    gallery.worked_hours = 39;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
@@ -553,7 +553,7 @@ angular.module('galleryApp', [])
       {
         id: 33,
         img: '33.png',
-        done: 50,
+        done: 90,
         title: {
           ro: "Blestemarea smochinului",
           en: "Jesus curses the fig tree with no fruit"
