22b728e9ac14003dd79e186e4a8cb3c7304ed818
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-03-14 22:12:07 +0200

    Update 27.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ angular.module('galleryApp', [])
     var gallery = this;
     gallery.language = "ro";
     gallery.work_start_date = new Date(2017,02,12);
-    gallery.worked_hours = 44;  // EDIT HERE
+    gallery.worked_hours = 45;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
@@ -474,8 +474,8 @@ angular.module('galleryApp', [])
       },
       {
         id: 27,
-        img: '27.jpg',
-        done: 20,
+        img: '27.png',
+        done: 70,
         title: {
           ro: "Vindecarea orbului din naștere",
           en: "Healing the man blind from birth"
