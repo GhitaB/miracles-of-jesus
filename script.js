4bc08780037f24a21678d643e81266e8dcf1320f
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-03-31 05:57:57 +0300

    Update 03.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ angular.module('galleryApp', [])
     var gallery = this;
     gallery.language = "ro";
     gallery.work_start_date = new Date(2017,02,12);
-    gallery.worked_hours = 57;  // EDIT HERE
+    gallery.worked_hours = 58;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
@@ -163,7 +163,7 @@ angular.module('galleryApp', [])
       {
         id: 3,
         img: '03.png',
-        done: 30,
+        done: 50,
         title: {
           ro: "Pescuirea minunată",
           en: "Catching a large number of fish"
