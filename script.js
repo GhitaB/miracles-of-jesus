ca0a752aeb45f0949ec2616408ddf065bdbac1f7
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-04-02 14:10:27 +0300

    Update 05.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ angular.module('galleryApp', [])
     var gallery = this;
     gallery.language = "ro";
     gallery.work_start_date = new Date(2017,02,12);
-    gallery.worked_hours = 60.5;  // EDIT HERE
+    gallery.worked_hours = 61;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
@@ -189,7 +189,7 @@ angular.module('galleryApp', [])
       {
         id: 5,
         img: '05.png',
-        done: 50,
+        done: 100,
         title: {
           ro: "Vindecarea robului sutașului roman",
           en: "Miracle of healing a centurion's servant"
