803acda094beb0be4b5d02e41b718f011fd9f393
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-04-02 17:33:09 +0300

    Update 24.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ angular.module('galleryApp', [])
     var gallery = this;
     gallery.language = "ro";
     gallery.work_start_date = new Date(2017,02,12);
-    gallery.worked_hours = 62.5;  // EDIT HERE
+    gallery.worked_hours = 63;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
@@ -435,8 +435,8 @@ angular.module('galleryApp', [])
       },
       {
         id: 24,
-        img: '24.jpg',
-        done: 30,
+        img: '24.png',
+        done: 100,
         title: {
           ro: "Vindecarea orbului la Betsaida",
           en: "Healing the blind man at Bethsaida"
