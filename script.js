1c8f93467599e1ad490c29f08ea431c6c2bb2b79
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-04-04 21:52:29 +0300

    Update 09.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ angular.module('galleryApp', [])
     var gallery = this;
     gallery.language = "ro";
     gallery.work_start_date = new Date(2017,02,12);
-    gallery.worked_hours = 70;  // EDIT HERE
+    gallery.worked_hours = 71;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
@@ -241,7 +241,7 @@ angular.module('galleryApp', [])
       {
         id: 9,
         img: '09.png',
-        done: 50,
+        done: 100,
         title: {
           ro: "Vindecarea slăbănogului",
           en: "Healing a paralyzed man"
