fe8e517590615eee913281655af367ddf4c237ec
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-04-07 06:32:24 +0300

    Update 10.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ angular.module('galleryApp', [])
     var gallery = this;
     gallery.language = "ro";
     gallery.work_start_date = new Date(2017,02,12);
-    gallery.worked_hours = 74.5;  // EDIT HERE
+    gallery.worked_hours = 75.5;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
@@ -254,7 +254,7 @@ angular.module('galleryApp', [])
       {
         id: 10,
         img: '10.png',
-        done: 50,
+        done: 75,
         title: {
           ro: "Vindecarea slăbănogului la Betesda",
           en: "Healing the paralytic at Bethesda"
