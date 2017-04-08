215b2e85eaffb5ad06a7e4bc5926073ed64c6528
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-04-08 23:04:31 +0300

    Update 15.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ angular.module('galleryApp', [])
     var gallery = this;
     gallery.language = "ro";
     gallery.work_start_date = new Date(2017,02,12);
-    gallery.worked_hours = 77;  // EDIT HERE
+    gallery.worked_hours = 77.5;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
@@ -319,7 +319,7 @@ angular.module('galleryApp', [])
       {
         id: 15,
         img: '15.png',
-        done: 30,
+        done: 50,
         title: {
           ro: "Învierea fiicei lui Iair",
           en: "Raising Jairus' daughter"
