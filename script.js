b58bb1d10ef9674f0a3ade6240f5fea897b1e936
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-04-09 22:11:16 +0300

    Update 13.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ angular.module('galleryApp', [])
     var gallery = this;
     gallery.language = "ro";
     gallery.work_start_date = new Date(2017,02,12);
-    gallery.worked_hours = 81.5;  // EDIT HERE
+    gallery.worked_hours = 82;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
@@ -293,7 +293,7 @@ angular.module('galleryApp', [])
       {
         id: 13,
         img: '13.png',
-        done: 50,
+        done: 100,
         title: {
           ro: "Potolirea furtunii",
           en: "Calming the storm"
