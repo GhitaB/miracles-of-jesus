d0c251690e1dc27cd5b0d97511866999fdfff140
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-04-30 13:43:39 +0300

    Update 18.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ angular.module('galleryApp', [])
     var gallery = this;
     gallery.language = "ro";
     gallery.work_start_date = new Date(2017,02,12);
-    gallery.worked_hours = 84.5;  // EDIT HERE
+    gallery.worked_hours = 85;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
@@ -358,7 +358,7 @@ angular.module('galleryApp', [])
       {
         id: 18,
         img: '18.png',
-        done: 50,
+        done: 100,
         title: {
           ro: "Vindecarea unui mut îndrăcit",
           en: "Healing the blind and dumb possessed"
