88d0b62d9a237e708591fa135bfca6b7b7240b2e
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-04-30 16:26:38 +0300

    Update 25.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ angular.module('galleryApp', [])
     var gallery = this;
     gallery.language = "ro";
     gallery.work_start_date = new Date(2017,02,12);
-    gallery.worked_hours = 87;  // EDIT HERE
+    gallery.worked_hours = 87.5;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
@@ -449,7 +449,7 @@ angular.module('galleryApp', [])
       {
         id: 25,
         img: '25.png',
-        done: 50,
+        done: 100,
         title: {
           ro: "Vindecarea unui copil lunatic",
           en: "Healing of the lunatic child"
