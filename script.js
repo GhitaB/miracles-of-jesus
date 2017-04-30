1f22b78d71e68b379cc9fc3f607c7b5e433feccd
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-04-30 15:59:00 +0300

    Update 23.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ angular.module('galleryApp', [])
     var gallery = this;
     gallery.language = "ro";
     gallery.work_start_date = new Date(2017,02,12);
-    gallery.worked_hours = 86.5;  // EDIT HERE
+    gallery.worked_hours = 87;  // EDIT HERE
 
     gallery.get_global_progress = function() {
       var index;
@@ -423,7 +423,7 @@ angular.module('galleryApp', [])
       {
         id: 23,
         img: '23.png',
-        done: 50,
+        done: 100,
         title: {
           ro: "Hrănirea celor patru mii de oameni",
           en: "The feeding of the four thousand"
